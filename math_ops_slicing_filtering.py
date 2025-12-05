d5e4829351f10bb354ec5bf4547c4c67d68853b6
ayazbabar831 <ayazbabar831@gmail.com>
2025-12-05 15:27:05 +0500

    added math ops slicing filtering and indexing file

---
+++ math_ops_slicing_filtering.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+a = np.random.randint(1,100,20)
+print(a)
+print(a.mean())
+print(a.sum())
+print(a.min())
+print(a.max())
+
+a_mul_2 = a * 2
+print(a_mul_2)
+
+print(a - 10)
+
+print(a[(a>60)]) #boolean filtering
+
+a2 = np.random.randint(1,10,(10,10))
+print(a2)
+print(a2[3,3])
+print(a2[3:7,3:7])
