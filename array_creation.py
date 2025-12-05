e3b645308f672f2267a6c112752ed044a24a1c7f
ayazbabar831 <ayazbabar831@gmail.com>
2025-12-05 14:59:48 +0500

    first commit

---
+++ array_creation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1 = np.zeros((3,3))
+array2 = np.ones((4,4))
+array3 = np.full((3,5),9)
+array4 = np.arange(1,10,3)
+print(array1)
+print(array2)
+print(array3)
+print(array4)
